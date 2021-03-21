83449cf528c652334cebf68d792277a7d45817a2
Guanhua Ding <william.ting.1116@gmail.com>
2021-03-21 16:12:54 +1000

    project idea

--- src/components/ProjectIdea.vue
+++ src/components/ProjectIdea.vue
@@ -1,89 +1,45 @@
 <template>
   <div class="relative bg-white py-16 sm:py-24">
     <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
-      <div class="relative sm:py-16 lg:py-0">
-        <div aria-hidden="true" class="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
-          <div class="absolute inset-y-0 right-1/2 w-full bg-gray-50 rounded-r-3xl lg:right-72"></div>
-          <svg class="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12" width="404" height="392" fill="none" viewBox="0 0 404 392">
-            <defs>
-              <pattern id="02f20b47-fd69-4224-a62a-4c9de5c763f7" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="404" height="392" fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
-          </svg>
-        </div>
-        <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
-          <div class="relative pt-64 pb-10 rounded-xl shadow-xl overflow-hidden" style="height: 700px;">
-            <img class="absolute inset-0 h-full w-full object-cover" src="@/assets/photo-3.png" alt="">
-          </div>
-          <figcaption class="mt-3 flex text-sm text-gray-500">
-            <!-- Heroicon name: solid/camera -->
-            <svg class="flex-none w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-            </svg>
-            <span class="ml-2">Game Science Studio, Hangzhou</span>
-          </figcaption>
-        </div>
-        <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
-          <div class="relative pt-64 pb-10 rounded-xl shadow-xl overflow-hidden" style="height: 586px;">
-            <img class="absolute inset-0 h-full w-full object-cover" src="@/assets/photo-4.png" alt="">
-          </div>
-          <figcaption class="mt-3 flex text-sm text-gray-500">
-            <!-- Heroicon name: solid/camera -->
-            <svg class="flex-none w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-            </svg>
-            <span class="ml-2 text-xs">Please note this is a Chinese company and the link is in Chinese. The above screenshot was captured with Google Translation.</span>
-          </figcaption>
-        </div>
-      </div>
-
       <div class="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
         <!-- Content area -->
         <div class="pt-12 sm:pt-16 lg:pt-20">
-          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Ideal Job</h2>
-          <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Game Engine Engineer</h3>
+          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Project Idea</h2>
+          <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Carpool APP</h3>
           <div class="mt-6 text-gray-500 space-y-6">
             <p class="text-lg">
-              My ideal job would be working as a software engineer or game engine engineer for a game company that I like. I was into gaming ever since I was a child, and working at a game company would really be a dream come true for me. This is an example of these jobs I hope I will be able to get one day.            </p>
-            <p class="text-base leading-7">
-              The company is called Game Science, and they have recently released a real time render of their first next gen game called Black Myth Wukong. Which is an action role-playing game based on the classical 16th-century Chinese novel Journey to the West.
+              Imagine an app like Uber but instead of finding someone who is a professional driver or someone who does it for a job, it will find other users who want to travel to the same destination as you. I believe it will be the perfect middle ground of driving by yourself and taking public transport.
             </p>
             <p class="text-base leading-7">
-              The job title is Unreal Engine 4 Graphics Engineer, which is what I want to do in the gaming industry. Although gaming is my passion, I am terrible at designing things or drawing graphics. As a game engine engineer I will be working closely with the game designers and graphics artists, using tools such as the UE4 game engine, C++ programming language. And I will join a team of passionable gamers to create the next gen games with the Chinese culture background.
+              While I was studying at QUT (Brisbane), I have always wanted an app that can help me find someone to carpool the commute from the Gold Coast to Brisbane and back. This will not only be more cost efficient than driving on your own, but also be more environmentally friendly than everyone driving their own car.
             </p>
             <p class="text-base leading-7">
-              My current skills are not inline with the job requirements at all. I had taken a few Unity courses in Udemy, and that’s all I have in terms of working with game engines. My plan is to study C++ and unreal game engine, and also complete the Bachelor of Information Technology degree from RMIT. Then I will be looking for a junior position working as a game engine engineer. With two to three years of experience gained, I will be ready to apply for this position or something similar.
+              In this app, everyone registered will be able to find someone to share a commute or offer others a ride. It can be a just one off trip for example from Brisbane to Sydney to offset the coast. Or it can be a recurring work commute Monday to Friday.
             </p>
-          </div>
-        </div>
-
-        <!-- Stats section -->
-        <div class="mt-10">
-          <dl class="grid grid-cols-2 gap-x-4 gap-y-8">
-            <div class="border-t-2 border-gray-100 pt-6">
-              <dt class="text-base font-medium text-gray-500">Position</dt>
-              <dd class="text-3xl font-extrabold tracking-tight text-gray-900">Game Engine Engineer</dd>
-            </div>
-
-            <div class="border-t-2 border-gray-100 pt-6">
-              <dt class="text-base font-medium text-gray-500">Degree</dt>
-              <dd class="text-3xl font-extrabold tracking-tight text-gray-900">Bachelor</dd>
-            </div>
-
-            <div class="border-t-2 border-gray-100 pt-6">
-              <dt class="text-base font-medium text-gray-500">Language</dt>
-              <dd class="text-3xl font-extrabold tracking-tight text-gray-900">C++</dd>
-            </div>
-
-            <div class="border-t-2 border-gray-100 pt-6">
-              <dt class="text-base font-medium text-gray-500">Game Engine</dt>
-              <dd class="text-3xl font-extrabold tracking-tight text-gray-900">Unreal Engine 4</dd>
-            </div>
-          </dl>
-          <div class="mt-10">
-            <a href="https://www.zhipin.com/job_detail/3cbe42230f4b0eac1nZ429m8EFdS.html?ka=comp_joblist_11" target="_blank" class="text-base font-medium text-indigo-600"> Find out more about this position <span aria-hidden="true">&rarr;</span> </a>
+            <p class="text-base leading-7">
+              What’s the difference between this app and Uber you may ask. Although they may sound similar, they are different. Instead of requesting a ride instantly, you will be more likely to find a shared ride or journey if you try to find a match beforehand. Another difference is distance. While Uber primarily focuses on short distance transport within a city, this app will be more focused on long distance travel. After all, that’s the whole idea of this project: saving money on long distance car journeys but still beats public transports on time.
+            </p>
+            <p class="text-base leading-7">
+              And the fundamental difference is that everyone on this app is a rider and a driver at the same time. You can offer your car to others, and you can also find someone that has a similar commute route as you and share the journey. We will also have features to set up recurring journeys and share the journey cost right within the app. Meaning no exchange of cash or money transfer needed.
+            </p>
+            <p class="text-base leading-7">
+              As for our business model, we will charge a percentage of the journey cost. Which includes the fuel cost, vehicle registration coast, depreciation of the vehicle and any toll occurred. For example if the journey cost a person $40 to complete. We will charge 10%, which brings the total cost to $44. However, the user is able to offset this cost to others by offering 3 seats in the car. Which brings the cost down to $11. Sure the user will need to pick up and drop off the other 3 riders. But we will have an algorithm in place to only match users with similar or very close start places and destinations.
+            </p>
+            <p class="text-base leading-7">
+              We will need to set up a cloud server with our database and backend program hosted. Because it is a mobile app, we will also need to develop the app for both the Android and iOS operating system. In today’s time and age everything is mobile first, I don’t think a web app is necessary. Although it would be a good idea to have some kind of product page to introduce our app and service.
+            </p>
+            <p class="text-base leading-7">
+              In terms of the actual tools and technologies, there are a lot of choices. Because I have relevant experiences I would of course choose tools and languages I am familiar with. For the backend I would use PHP with Laravel, it is the choice of many large corporations. I would like to use Flutter for the frontend, it is a new emerging mobile UI framework that can build for both Android and iOS with the same code base. For cloud service and hosting, AWS is an obvious choice. Plus it is the one I had the most experience with.
+            </p>
+            <p class="text-base leading-7">
+              To make this project possible we need a team of developers as well as people from other professional: designers, product managers, sales, legal, and human resources to just name a few. But if we are just focused on the technology side of things, we will need developers who are experienced with the Laravel framework, flutter UI, and AWS engineers.
+            </p>
+            <p class="text-base leading-7">
+              To bring this project into reality does not require a lot of cutting edge technology. Everything we could want is already there. We just need to find the right tools and right people. The challenge will be on how to make this a viable business. Furthermore, legal and legislation is another conser when it comes to public roads and motor vehicles.
+            </p>
+            <p class="text-base leading-7">
+              There are a lot of ride sharing apps out there, but none of them offer what this project is trying to achieve: share your commute and long distance journey with others to offset the cost as well as reduce the carbon footprint. If this project is successful, it will be a viable business model and not a lot of competition.
+            </p>
           </div>
         </div>
       </div>
