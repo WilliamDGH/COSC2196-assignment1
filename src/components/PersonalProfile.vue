9bc5b0f94bc0c18a343dc54d17b7fded1ac376ff
Guanhua Ding <william.ting.1116@gmail.com>
2021-03-21 14:48:33 +1000

    personality

--- src/components/PersonalProfile.vue
+++ src/components/PersonalProfile.vue
@@ -31,27 +31,43 @@
     <div class="relative px-4 sm:px-6 lg:px-8">
       <div class="text-lg max-w-prose mx-auto">
         <h1>
-          <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">GUANHUA DING</span>
-          <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Software Developer</span>
+          <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Personality Test</span>
+          <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Personal Profile</span>
         </h1>
       </div>
       <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-        <p>
-          I am a junior software developer who is passionate about the ever changing nature of the tech industry. After trying to teach myself how to code for over a year or so via online courses such as Udemy I decided to quit retail to make a full time commitment to becoming a Web Developer.
-        </p>
-        <p>
-          I Grew up in China, and have spent the last 8 years in Australia. This experience gives me a more broad view of different cultures and lifestyles. I speak Chinese and English, and follow the trend in the tech industry in both China and the west.
-        </p>
         <figure>
-          <img class="w-full rounded-lg" src="@/assets/photo-1.jpeg" alt="" width="1310" height="873">
-          <figcaption class="mt-3 flex text-sm text-gray-500">
-            <!-- Heroicon name: solid/camera -->
-            <svg class="flex-none w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-            </svg>
-            <span class="ml-2"> Me and my son Lucas in Santorini, Greece</span>
-          </figcaption>
+          <img class="w-full rounded-lg" src="@/assets/photo-5.png" alt="" width="1310" height="873">
         </figure>
+
+        <p>
+          16 personalities test from <a href="www.16personalities.com" target="_blank">www.16personalities.com</a>.
+        </p>
+
+        <figure>
+          <img class="w-full rounded-lg" src="@/assets/photo-6.png" alt="" width="1310" height="873">
+        </figure>
+
+        <p>
+          What's your Learning Style test from <a href="http://www.educationplanner.org/students/self-assessments/learning-styles.shtml" target="_blank">Educationplanner.org</a>.
+        </p>
+
+        <figure>
+          <img class="w-full rounded-lg" src="@/assets/photo-7.png" alt="" width="1310" height="873">
+        </figure>
+
+        <p>
+          Big Five Personality Test from <a href="https://openpsychometrics.org/tests/IPIP-BFFM/" target="_blank">Open-Source Psychometrics Project</a>.
+        </p>
+
+        <p>
+          From the test results, it is clear that I have an interval personality. I don’t like to start conversations and talk to people about their personal lifes. But that doesn’t mean I don’t care about others, I just found it difficult to start chatting about things. However, when someone does tell me about their problems, I am more than happy to help them as best as I can.
+        </p>
+        <p>
+          When working in a team, I won’t be too proactive. I don’t like to take the leading role, and organise everything. However, I will provide my skills and knowledge to others. And I am committed to finish my part of the work.        </p>
+        <p>
+          When forming a team, it will be better for me and others that I find teammates who like to take the leading role. And also people who are more into communication and have better intel personal skills.
+        </p>
       </div>
     </div>
   </div>
